3779422f4029b16b9d61d6410b2767b948103149
thanh <miktenthanh@gmail.com>
2023-10-06 18:39:46 +0700

    frist-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MyComponent from "./components/Mycomponents";
+function App() {
+  return (
+    <>
+      <h2>cc</h2>
+      <MyComponent></MyComponent>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Mycomponents.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  // jsx
+  render() {
+    return (
+      <div>
+        {Math.random()}
+        <h1>Do Cam Ly</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
