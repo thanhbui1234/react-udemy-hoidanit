2ec9a8d3d6ed50c3992a8d5cbab08d154905e160
thanh <miktenthanh@gmail.com>
2023-10-11 16:52:29 +0700

    update 10/11

--- src/components/Mycomponents.jsx
+++ src/components/Mycomponents.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import Hello from "./Hello";
+const Mycomponents2 = () => {
+  const listUser = [
+    { id: 1, name: "thanh" },
+    { id: 1, name: "thanh" },
+    { id: 12, name: "thanh" },
+    { id: 13, name: "thanh" },
+    { id: 4, name: "thanh" },
+  ];
 
-class MyComponent extends React.Component {
-  // jsx
-  render() {
-    return (
-      <div>
-        {Math.random()}
-        <h1>Do Cam Ly</h1>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Hello listUser={listUser} />
+    </div>
+  );
+};
 
-export default MyComponent;
+export default Mycomponents2;
